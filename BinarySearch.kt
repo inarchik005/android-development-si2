af177a785d8433f2a96c41ecd7c1ecae1dc3a078
inara <i_assylzhanova@kbtu.kz>
2026-09-21 03:51:45 +0500

    Implement binary search algorithm in Kotlin

---
+++ BinarySearch.kt
@@ -0,0 +1,19 @@
+
+class Solution {
+    fun search(nums: IntArray, target: Int): Int {
+        var left = 0
+        var right = nums.size - 1
+
+        while (left <= right) {
+            val mid = left + (right - left) / 2
+
+            when {
+                nums[mid] == target -> return mid
+                nums[mid] < target -> left = mid + 1
+                else -> right = mid - 1
+            }
+        }
+
+        return -1
+    }
+}
